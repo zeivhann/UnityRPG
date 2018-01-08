f1aa31912cd2a2e0fc0463aa40a37ed488b3fc48
zeivhann <alexsilcott@gmail.com>
2018-01-08 12:09:15 -0500

    Added mesh conversion and default equipment for player

--- Assets/Scripts/GameManager/EquipmentManager.cs
+++ Assets/Scripts/GameManager/EquipmentManager.cs
@@ -18,6 +18,7 @@ public class EquipmentManager : MonoBehaviour {
     // Holds the mesh for the player body so we can reference it
     public SkinnedMeshRenderer targetMesh;
 
+    public Equipment[] defaultEquipment;
     Equipment[] currentEquipment; // Items that are currently equipped
     SkinnedMeshRenderer[] meshes; // Meshes of currently equipped items
 
@@ -35,6 +36,8 @@ public class EquipmentManager : MonoBehaviour {
         int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
         currentEquipment = new Equipment[numSlots];
         meshes = new SkinnedMeshRenderer[numSlots];
+
+        EquipDefaultItems();
     }
 
     // Equip item
@@ -43,19 +46,16 @@ public class EquipmentManager : MonoBehaviour {
         // Gets the index of the item in the list of Enums
         int slotIndex = (int) newItem.equipSlot;
 
-        Equipment oldItem = null;
-
-        // If an item is already equipped, we want to swap the items and 
-        if (currentEquipment[slotIndex] != null)
-        {
-            oldItem = currentEquipment[slotIndex];
-            inventory.Add(oldItem);
-        }
+        // Handles unequipping current item
+        Equipment oldItem = Unequip(slotIndex);
 
         if (onEquipmentChanged != null)
         {
             onEquipmentChanged.Invoke(newItem, oldItem);
         }
+
+        // Set the blendshapes for the meshes on the player's equipped items
+        this.SetEquipmentBlendShapes(newItem, 100);
 
         // Put the new item into the currentEquipment array
         currentEquipment[slotIndex] = newItem;
@@ -72,8 +72,19 @@ public class EquipmentManager : MonoBehaviour {
         meshes[slotIndex] = newMesh;
     }
 
-    // Unequip an item
-    public void Unequip (int slotIndex)
+    // Set the blendshape for a piece of equipment so it forms properly to the body
+    void SetEquipmentBlendShapes (Equipment item, int weight)
+    {
+        // Loop through each blendshape in the EquipmentMeshRegion enum that exists in the current coveredMeshRegions array
+        foreach (EquipmentMeshRegion blendShape in item.coveredMeshRegions)
+        {
+            // casting blendShape to (int) returns the index of the enum
+            targetMesh.SetBlendShapeWeight((int)blendShape, weight);
+        }
+    }
+
+    // Unequip an item; returns the item that was unequipped
+    public Equipment Unequip (int slotIndex)
     {
         if (currentEquipment[slotIndex] != null)
         {
@@ -84,6 +95,10 @@ public class EquipmentManager : MonoBehaviour {
             }
 
             Equipment oldItem = currentEquipment[slotIndex];
+
+            // set blendShapes to zero because mesh is removed from player
+            this.SetEquipmentBlendShapes(oldItem, 0);
+
             inventory.Add(oldItem);
 
             currentEquipment[slotIndex] = null;
@@ -92,7 +107,9 @@ public class EquipmentManager : MonoBehaviour {
             {
                 onEquipmentChanged.Invoke(null, oldItem);
             }
+            return oldItem;
         }
+        return null;
     }
 
     public void UnequipAll ()
@@ -100,6 +117,18 @@ public class EquipmentManager : MonoBehaviour {
         for (int i = 0; i < currentEquipment.Length; i++)
         {
             Unequip(i);
+        }
+
+        // Equip player with default items (meshes)
+        EquipDefaultItems();
+    }
+
+    // Equips starting items
+    void EquipDefaultItems()
+    {
+        foreach (Equipment item in this.defaultEquipment)
+        {
+            Equip(item);
         }
     }
 
---
+++ Assets/Scripts/Helper/ConvertToRegularMesh.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToRegularMesh : MonoBehaviour {
+
+    // Add this selection in the editor (gear icon)
+    [ContextMenu("Convert to regular mesh")]
+	void Convert ()
+    {
+        // Get SkinnedMeshRenderer component of the game object
+        SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        
+        // Add a Mesh Renderer to the game object
+        MeshRenderer meshRenderer = this.gameObject.AddComponent<MeshRenderer>();
+
+        // Add a Mesh Filter to the game object
+        MeshFilter meshFilter = this.gameObject.AddComponent<MeshFilter>();
+
+
+        meshFilter.sharedMesh = skinnedMeshRenderer.sharedMesh;
+        meshRenderer.sharedMaterials = skinnedMeshRenderer.sharedMaterials;
+
+        // Destroy the Mesh Renderer component and this script on the object
+        DestroyImmediate(skinnedMeshRenderer);
+        DestroyImmediate(this);
+    }
+}
